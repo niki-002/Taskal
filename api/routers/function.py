32be827a54c0806c3e49a7a554d21bdd04fbe87d
niki-002 <nichinara.niki02@gmail.com>
2026-04-02 00:58:35 +0900

    ver2.2更新（エンドポイント編集）

--- api/routers/function.py
+++ api/routers/function.py
@@ -7,18 +7,18 @@ from schemas import TaskCreate, TaskReadResponse
 
 router = APIRouter()
 
-@router.get("/")
+@router.get("/tasks")
 def get_tasks(database: Session = Depends(get_database)):
     return crud.get_tasks(database)
 
-@router.get("/{task_id}", response_model=TaskReadResponse)
+@router.get("/tasks/{task_id}", response_model=TaskReadResponse)
 def get_task(task_id: int, database: Session = Depends(get_database)):
     return crud.get_task(task_id, database)
 
-@router.post("/")
+@router.post("/tasks")
 def create_task(new_task: TaskCreate, database: Session = Depends(get_database)):
     return crud.create_task(new_task, database)
 
-@router.delete("/{task_id}")
+@router.delete("/tasks/{task_id}")
 def delete_task(task_id: int, database: Session = Depends(get_database)):
     return crud.delete_task(task_id, database)
