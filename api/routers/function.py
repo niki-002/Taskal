0797eb4565f346d1ffa65ae26117e9c058209ed5
niki-002 <nichinara.niki02@gmail.com>
2026-04-02 00:54:10 +0900

    ver2.1更新

--- api/routers/function.py
+++ api/routers/function.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 
-from ..database import get_database
-from .. import crud
-from ..schemas import TaskCreate, TaskReadResponse
+from database import get_database
+import crud
+from schemas import TaskCreate, TaskReadResponse
 
 router = APIRouter()
 
@@ -15,7 +15,7 @@ def get_tasks(database: Session = Depends(get_database)):
 def get_task(task_id: int, database: Session = Depends(get_database)):
     return crud.get_task(task_id, database)
 
-@router.post("/", response_model=TaskCreateResponse)
+@router.post("/")
 def create_task(new_task: TaskCreate, database: Session = Depends(get_database)):
     return crud.create_task(new_task, database)
 
