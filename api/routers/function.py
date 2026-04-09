5b0eff74df450f655d2dc9d795bf717f0af27eb6
niki-002 <nichinara.niki02@gmail.com>
2026-04-09 21:54:28 +0900

    ver4.0 HTTPException修正

--- api/routers/function.py
+++ api/routers/function.py
@@ -19,5 +19,5 @@ def create_task(new_task: TaskCreate, database: Session = Depends(get_database))
 def delete_task(task_id: int, database: Session = Depends(get_database)):
     task = crud.delete_task(task_id, database)
     if not task:
-        raise HTTPException(status_code=404, detail="task not found")
+        raise HTTPException(status_code=404, detail="task not found.")
     return None 
