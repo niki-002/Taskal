2c2becf374fac8fe933aba2dc4d124117ad33c6b
niki-002 <nichinara.niki02@gmail.com>
2026-04-09 22:12:34 +0900

    ver4.0 ORMモデルの親クラス定義変更

--- api/database.py
+++ api/database.py
@@ -2,20 +2,26 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+# 環境変数からデータベースのURLを取得
 DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL is not set.")
 
+# データベース接続の窓口
 database_engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
+# 1リクエスト中のDB操作単位
 database_session = sessionmaker(autoflush=False, 
                                 autocommit=False, 
                                 bind=database_engine)
 
+<<<<<<< HEAD
+=======
+# fastapi依存注入の関数
+>>>>>>> 41356ec (ver4.0 ORMモデルの親クラス定義変更)
 def get_database():
     database = database_session()
     try:
         yield database
     finally:
         database.close()
-# db_sessionが1リクエスト中のDB操作単位
