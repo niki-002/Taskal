0949af3200ade0ee4c7d4942ca53bf01e0386846
niki-002 <nichinara.niki02@gmail.com>
2026-04-09 22:20:36 +0900

    ver4.0 モデル定義変更

--- api/models.py
+++ api/models.py
@@ -1,12 +1,20 @@
+<<<<<<< HEAD
 from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy.orm import declarative_base
 
 class Base(declarative_base):
+=======
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
+
+# ORMモデルの親クラス
+class Base(DeclarativeBase):
+>>>>>>> 883a560 (ver4.0 モデル定義変更)
     pass
 
 class Task(Base):
     __tablename__ = "tasks"
 
-    id = Column(Integer, primary_key=True)
-    title = Column(String(200), nullable=False)
-    done_flag = Column(Boolean, nullable=False, default=False)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(200), nullable=False)
+    done_flag: Mapped[bool] = mapped_column(nullable=False, default=False)
