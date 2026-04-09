81bc292dc8fcdf26b9dfcf94bb6b34e96e1bf2b6
niki-002 <nichinara.niki02@gmail.com>
2026-04-09 21:13:19 +0900

    ver4.0 スキーマ定義変更

--- api/schemas.py
+++ api/schemas.py
@@ -2,11 +2,8 @@ from pydantic import BaseModel, Field
 
 # ベーススキーマ
 class TaskBase(BaseModel):
-    title: str | None = Field(None, example="書類チェック")
+    title: str = Field(..., min_length=1, max_length=200, example="書類チェック")
 
 # リクエストスキーマ
 class TaskCreate(TaskBase):
     pass
-
-    class Config:
-        orm_mode = True
