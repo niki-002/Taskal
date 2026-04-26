c7c15daa305db63d4a32c3b8adf6be13e861e2fc
niki-002 <nichinara.niki02@gmail.com>
2026-04-26 17:41:20 +0900

    add: オーナーIDカラム追加

--- api/models/task.py
+++ api/models/task.py
@@ -1,5 +1,5 @@
 # データベース構造の定義
-from sqlalchemy import String
+from sqlalchemy import String, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 from datetime import date
 
@@ -10,7 +10,15 @@ class Task(Base):
     __tablename__ = "tasks"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    title: Mapped[str] = mapped_column(String(200), nullable=False)
+    title: Mapped[str] = mapped_column(
+        String(200),
+        index=True,
+        nullable=False
+        )
+    owner_id: Mapped[int] = mapped_column(ForeignKey("users.id"))  # ForeignKeyは引数に定められたカラムに存在する値だけを入れられるようにするための要素
     description: Mapped[str] = mapped_column(String(1000))
     limit: Mapped[date] = mapped_column() 
-    done_flag: Mapped[bool] = mapped_column(nullable=False, default=False)
+    done_flag: Mapped[bool] = mapped_column(
+        nullable=False,
+        default=False
+        )
