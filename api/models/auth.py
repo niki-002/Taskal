22cad3243e32c6d15700e7298b2a0be6b38054e1
niki-002 <nichinara.niki02@gmail.com>
2026-04-26 17:39:04 +0900

    fix: Userテーブルモデル修正

--- api/models/auth.py
+++ api/models/auth.py
@@ -1,12 +1,19 @@
 from sqlalchemy.orm import Mapped, mapped_column
-
 from .Base import Base
 
 
 class User(Base):
-    __tablename__ = "user"
+    __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    username: Mapped[str] = mapped_column(nullable=False)
-    emailaddress: Mapped[str] = mapped_column(nullable=False)
-    hashpass: Mapped[str] = mapped_column(nullable=False)
+    username: Mapped[str] = mapped_column(
+        unique=True,
+        index=True,
+        nullable=False
+        )
+    email: Mapped[str] = mapped_column(
+        unique=True,
+        index=True,
+        nullable=False
+        )
+    hashed_password: Mapped[str] = mapped_column(nullable=False)
